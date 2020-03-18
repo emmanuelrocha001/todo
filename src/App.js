70752cc1e8950860a0d326cde73be2059a2c49d2
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-18 00:09:43 -0500

    search bar added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,81 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class TaskBoard extends React.Component {
+
+};
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      tasks: []
+    }
+
+    // bind function
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+
+  }
+
+
+  handleChange(event) {
+
+    this.setState({userInput: event.target.value});
+
+  }
+
+
+
+  handleKeyPress(event) {
+    if( event.key === "Enter" ) {
+      alert( " \"" + this.state.userInput + " \"" );
+      event.preventDefault();
+    } else {
+    }
+  }
+
+
+  render() {
+
+    return (
+    <form >
+      <input onKeyDown={this.handleKeyPress} className="SearchBar" type="text" value={this.state.UserInput} onChange={this.handleChange} />
+
+
+
+    <h1>{this.state.userInput}</h1>
+    </form>
+    );
+
+  }
+};
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      tasks: []
+    }
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
+
+  }
+
+};
 
 export default App;
