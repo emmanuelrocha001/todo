4bd4351aa7529b48c471e3a5c794685053229d23
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-21 13:01:19 -0500

    Updates

---
+++ precache-manifest.c51a7458f4c09d521fd7b028b1f7f73b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f368c409ac0ea168de5042f28d04610",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "cf6947389ab1976f4c45",
+    "url": "/todo/static/css/main.3bedae8d.chunk.css"
+  },
+  {
+    "revision": "53f9da8a693db64aa0b1",
+    "url": "/todo/static/js/2.e74fada5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo/static/js/2.e74fada5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6947389ab1976f4c45",
+    "url": "/todo/static/js/main.e1e17269.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  },
+  {
+    "revision": "dc8a298218b01cb39958e045e14036f8",
+    "url": "/todo/static/media/quit.dc8a2982.png"
+  }
+]);
